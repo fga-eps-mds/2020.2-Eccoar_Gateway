660bcb1a04729862d871ae90493487b0ba50200f
Victor-Buendia <victorbuendia03@gmail.com>
2021-03-23 22:15:41 -0300

    Create route to list complaints by userId

--- src/api/complaint/ComplaintProxy.ts
+++ src/api/complaint/ComplaintProxy.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { response, Response, Request } from 'express';
+import { response, Response, Request, json } from 'express';
 
 
 export class ComplaintProxy {
@@ -42,5 +42,30 @@ export class ComplaintProxy {
         } catch(err) {
             return resp.status(err.response.status).json(err.response.data);
         }
+    }
+
+    async addVote(req:Request, resp: Response): Promise<Response>{
+        try {
+            const res = await axios.post(this.path + '/vote/add', req.body);
+            return resp.sendStatus(res.status);
+        } catch(err) {
+            return resp.status(err.response.status).json(err.response.data);
+        }
+    }
+
+    async listVote(req:Request, resp: Response): Promise<Response> {
+        return new Promise(resolve => {
+            axios.get(this.path + '/vote/list', {
+                params: {
+                    userId: Number(req.query.userId),
+                    skip: Number(req.query.skip),
+                    take: Number(req.query.take)
+                }
+            }).then(response => {
+               return resp.status(response.status).json(response.data);
+            }).catch((err) => {
+               return resp.status(err.response.status).json(err.response.data);
+            });
+        });
     }
 }
