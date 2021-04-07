cf09b63f016c5d27e3dfc6007ea72f15293816bb
Victor-Buendia <victorbuendia03@gmail.com>
2021-04-07 12:03:34 -0300

    Setup supertests
    Co-authored-by: Matheus Blanco <msallesblanco@gmail.com>

--- test/ComplaintProxy.test.ts
+++ test/ComplaintProxy.test.ts
@@ -1,5 +1,18 @@
-describe('empty test suite', () => {
-	test('true to be true', () => {
-		expect(true).toBe(true);
-	});
+import app from '../src/server'; // Link to your server file
+import * as request from 'supertest';
+
+app.get('/test', async (req, res) => {
+    res.json({message: 'pass!'});
+  });
+
+describe("test endpoint", () => {
+
+    it('gets the test endpoint', async done => {
+        const response = await request(app).get('/test');
+      
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe('pass!');
+        done();
+      });
+
 });
