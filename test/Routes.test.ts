14a1566f2fb555f1adf99cd667ffacbe5481050d
MatheusBlanco <msallesblanco@gmail.com>
2021-04-09 20:57:31 -0300

    Adding more tests

--- test/Routes.test.ts
+++ test/Routes.test.ts
@@ -21,6 +21,13 @@ describe("test get ping endpoint", () => {
     expect(result.status).toEqual(200);
     expect(result.body).toContain(pingMock.body);
   });
+
+  it('Get pong from ping in reports route', async () => {
+    jest.spyOn(axios, 'get').mockImplementationOnce(() => Promise.resolve(pingMock));
+    const result = await request(app).get('/api/reports/ping').send({});
+    expect(result.status).toEqual(200);
+    expect(result.body).toContain(pingMock.body);
+  });
 });
 
 describe("test complaints route", () => {
@@ -38,7 +45,36 @@ describe("test complaints route", () => {
     jest.spyOn(axios, 'post').mockImplementationOnce(() => Promise.resolve(mockStatus));
     const result = await request(app).post('/api/complaint/create').send({});
     expect(result.status).toEqual(200);
+  });
 
+  it('Test adds vote API', async () => {
+    jest.spyOn(axios, 'post').mockImplementationOnce(() => Promise.resolve(mockStatus));
+    const result = await request(app).post('/api/vote/add').send({});
+    expect(result.status).toEqual(200);
+  });
+
+  it('Test lists votes API', async () => {
+    jest.spyOn(axios, 'get').mockImplementationOnce(() => Promise.resolve(mockStatus));
+    const result = await request(app).get('/api/vote/list').send({});
+    expect(result.status).toEqual(200);
+  });
+
+  it('Test gets complaints with votes API', async () => {
+    jest.spyOn(axios, 'get').mockImplementationOnce(() => Promise.resolve(mockStatus));
+    const result = await request(app).get('/api/complaint/votes').send({});
+    expect(result.status).toEqual(200);
   });
 });
 
+describe("test reports route", () => {
+  const mockStatus = {
+    status: 200
+  };
+
+  it('Test create reports API', async () => {
+    jest.spyOn(axios, 'post').mockImplementationOnce(() => Promise.resolve(mockStatus));
+    const result = await request(app).post('/api/report/create').send({});
+    expect(result.status).toEqual(200);
+  });
+
+});
