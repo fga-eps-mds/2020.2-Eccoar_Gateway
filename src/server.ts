a089e5dd8fa273a0901fb3d44312cc38d5ee2380
MatheusBlanco <msallesblanco@gmail.com>
2021-04-10 12:12:29 -0300

    Fixing requests as per review

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,8 @@ const PORT = process.env.APP_PORT || 5000;
 app.use(express.json());
 app.use(morgan('combined'));
 app.use(cors());
-app.use(handleErrors);
 app.use(routes);
+app.use(handleErrors);
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
