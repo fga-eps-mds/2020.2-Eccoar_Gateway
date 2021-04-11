91cc358b8982399256360860e76189f65b1201a2
Sabanai104 <gabrielsabanaitrindade@gmail.com>
2021-04-11 01:01:27 -0300

    Limite the image length
    Co-authored-by: Arthur Matos <git.arthurmatos@gmail.com>
    Co-authored-by: Saleh Kader <saleh.nazih.dev@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import routes from './routes';
 const app = express();
 const PORT = process.env.APP_PORT || 5000;
 
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(morgan('combined'));
 app.use(cors());
 app.use(routes);
