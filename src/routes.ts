b381edc2cebaf4f35fdf0f07590006b91fe4c742
Victor-Buendia <victorbuendia03@gmail.com>
2021-04-13 22:00:53 -0300

    Fix delete complaint route name

--- src/routes.ts
+++ src/routes.ts
@@ -48,6 +48,10 @@ routers.get('/api/complaints/votes', async (req: Request, resp: Response) => {
 	return await complaintProxy.getComplaintWithVote(req, resp);
 });
 
+routers.delete('/api/complaint/delete', async (req: Request, resp: Response) => {
+	return await complaintProxy.deleteComplaint(req, resp);
+});
+
 routers.post('/api/mailer/send', async (req: Request, resp: Response) => {
 	try {
 		const reportRequest = {} as Request;
